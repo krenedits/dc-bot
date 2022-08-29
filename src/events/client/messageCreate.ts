30a239651a923740e60f258717ca4bd7e5fda4e3
Krenedits Ferenc <fefe.kre@gmail.com>
2022-08-30 00:22:34 +0200

    taught bot to speak

--- src/events/client/messageCreate.ts
+++ src/events/client/messageCreate.ts
@@ -1,12 +1,26 @@
-import { Message } from 'discord.js';
+import { ChannelType, Message, VoiceBasedChannel } from 'discord.js';
 import { TClient } from '../..';
+import { getVoiceStream } from '../../tts/index.js';
+import { connectToVoiceChannel } from '../../events/client/voiceStateUpdate.js';
+
+const handleSpeak = (message: Message, client: TClient) => {
+    const { author } = message;
+    const channel = 
+    client.channels.cache.find((channel) => channel.type === ChannelType.GuildVoice && channel.members.has(author.id));
+    if (channel?.type !== ChannelType.GuildVoice) return;
+    const stream = getVoiceStream(message.content.split(' ').slice(1).join(' '));
+    connectToVoiceChannel(channel, client, stream);
+}
 
 const event = {
     name: 'messageCreate',
     async execute(client: TClient, message: Message) {
-        if (message.author.bot) return;
         const words = message.content.split(' ');
         const to = words[0];
+        if (to === 'speak') {
+            return handleSpeak(message, client);
+        }
+        if (message.author.bot) return;
         client.users.cache.get(to)?.send(words.slice(1).join(' ') || 'xd');
     },
 };
